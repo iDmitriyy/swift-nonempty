90f76734447ec51550e5cf5d287b5e2ad4f2059a
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-22 04:12:38 +0300

    head tail init

--- Sources/NonEmpty/NonEmpty+Spi.swift
+++ Sources/NonEmpty/NonEmpty+Spi.swift
@@ -6,12 +6,14 @@
 //
 
 extension NonEmpty {
+  // @inlinable @inline(__always)
   @_spi(NonEmptyExternallyExtendable)
   public mutating func _mutateRawValue(_ transform: (inout Collection) -> Void) {
     transform(&rawValue)
   }
   
   @_spi(NonEmptyExternallyExtendable)
+  @inlinable @inline(__always)
   public var _rawValueMutable: Collection {
     get { rawValue }
     _modify {
@@ -20,10 +22,21 @@ extension NonEmpty {
   }
     
   @_spi(NonEmptyExternallyExtendable)
+  @inlinable @inline(__always)
   public init(_nonEmptyRawValueBuilder: () -> Collection) {
     let assumedNonEmptyRawValue = _nonEmptyRawValueBuilder()
-    precondition(!assumedNonEmptyRawValue.isEmpty,
-                 "Can't construct \(Self.self) from _nonEmptyRawValueBuilder that returns empty collection")
+//    precondition(!assumedNonEmptyRawValue.isEmpty,
+//                 "Can't construct \(Self.self) from _nonEmptyRawValueBuilder that returns empty collection")
     self.init(rawValue: assumedNonEmptyRawValue)!
+  }
+}
+
+extension NonEmpty where Collection: RangeReplaceableCollection {
+  @inlinable @inline(__always)
+  public init(head: Element, tail: [Element]) {
+    var coll = Collection()
+    coll.append(head)
+    coll.append(contentsOf: tail)
+    self.init(rawValue: coll)!
   }
 }
