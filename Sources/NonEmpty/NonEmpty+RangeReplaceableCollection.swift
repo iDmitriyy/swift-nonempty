1db3200f8755363be843b3dc6199c2188a1905c2
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2025-12-11 00:40:57 +0300

    head tail init overloads

--- Sources/NonEmpty/NonEmpty+RangeReplaceableCollection.swift
+++ Sources/NonEmpty/NonEmpty+RangeReplaceableCollection.swift
@@ -1,11 +1,45 @@
 // NB: `NonEmpty` does not conditionally conform to `RangeReplaceableCollection` because it contains destructive methods.
 extension NonEmpty where Base: RangeReplaceableCollection {
   @inlinable @inline(__always)
-  public init(_ head: Element, _ tail: Element...) {
+  public init(_ a: Element) {
     var result = Base()
-    result.reserveCapacity(tail.count + 1)
+    result.reserveCapacity(1) // ? is it ok to reserve for 1 element
+    result.append(a)
+    self.init(_unsafeAssumedNonEmpty: result)
+  }
+  
+  @inlinable @inline(__always)
+  public init(_ a: Element, _ b: Element) {
+    var result = Base()
+    result.reserveCapacity(2)
         
-    result.append(head)
+    result.append(a)
+    result.append(b)
+    
+    self.init(_unsafeAssumedNonEmpty: result)
+  }
+  
+  @inlinable @inline(__always)
+  public init(_ a: Element, _ b: Element, _ c: Element) {
+    var result = Base()
+    result.reserveCapacity(3)
+        
+    result.append(a)
+    result.append(b)
+    result.append(c)
+    
+    self.init(_unsafeAssumedNonEmpty: result)
+  }
+  
+  @inlinable @inline(__always)
+  public init(_ a: Element, _ b: Element, _ c: Element, _ d: Element, _ tail: Element...) {
+    var result = Base()
+    result.reserveCapacity(4 + tail.count)
+        
+    result.append(a)
+    result.append(b)
+    result.append(c)
+    result.append(d)
     result.append(contentsOf: tail)
     
     self.init(_unsafeAssumedNonEmpty: result)
